54cee0788408b5652ac9915e63e44e9122c0fe2b
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-02-08 15:31:37 +0600

    string

--- w3schools_python/data_types/string.py
+++ w3schools_python/data_types/string.py
@@ -37,3 +37,129 @@ print("expensive" not in txt)
 txt = "The best things in life are free!"
 if "expensive" not in txt:
     print("Yes, 'expensive' is NOT present.")
+
+# Slicing
+# You can return a range of characters by using the slice syntax.
+# Specify the start index and the end index, separated by a colon, to return a part of the string.
+# Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+print(b[2:5])
+# Note: The first character has index 0.
+
+# Slice From the Start
+# Get the characters from the start to position 5 (not included):
+b = "Hello, World!"
+print(b[:5])
+
+# Slice To the End
+# By leaving out the end index, the range will go to the end:
+
+# Example
+# Get the characters from position 2, and all the way to the end:
+
+b = "Hello, World!"
+print(b[2:])
+
+# Negative Indexing
+b = "Hello, World!"
+print(b[-5:-2])
+
+# Upper Case
+a = "Hello, World!"
+print(a.upper())
+
+# Lower Case
+a = "Hello, World!"
+print(a.lower())
+
+# Remove Whitespace
+a = " Hello, World! "
+print(a.strip())  # returns "Hello, World!"
+
+# Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# Split String
+a = "Hello, World!"
+print(a.split(","))  # returns ['Hello', ' World!']
+
+# String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# String Format
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+
+# Escape Character
+txt = "We are the so-called \"Vikings\" from the north."
+"""
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+"""
+
+# String Methods
+"""
+capitalize()	Converts the first character to upper case
+casefold()	Converts string into lower case
+center()	Returns a centered string
+count()	Returns the number of times a specified value occurs in a string
+encode()	Returns an encoded version of the string
+endswith()	Returns true if the string ends with the specified value
+expandtabs()	Sets the tab size of the string
+find()	Searches the string for a specified value and returns the position of where it was found
+format()	Formats specified values in a string
+format_map()	Formats specified values in a string
+index()	Searches the string for a specified value and returns the position of where it was found
+isalnum()	Returns True if all characters in the string are alphanumeric
+isalpha()	Returns True if all characters in the string are in the alphabet
+isdecimal()	Returns True if all characters in the string are decimals
+isdigit()	Returns True if all characters in the string are digits
+isidentifier()	Returns True if the string is an identifier
+islower()	Returns True if all characters in the string are lower case
+isnumeric()	Returns True if all characters in the string are numeric
+isprintable()	Returns True if all characters in the string are printable
+isspace()	Returns True if all characters in the string are whitespaces
+istitle()	Returns True if the string follows the rules of a title
+isupper()	Returns True if all characters in the string are upper case
+join()	Joins the elements of an iterable to the end of the string
+ljust()	Returns a left justified version of the string
+lower()	Converts a string into lower case
+lstrip()	Returns a left trim version of the string
+maketrans()	Returns a translation table to be used in translations
+partition()	Returns a tuple where the string is parted into three parts
+replace()	Returns a string where a specified value is replaced with a specified value
+rfind()	Searches the string for a specified value and returns the last position of where it was found
+rindex()	Searches the string for a specified value and returns the last position of where it was found
+rjust()	Returns a right justified version of the string
+rpartition()	Returns a tuple where the string is parted into three parts
+rsplit()	Splits the string at the specified separator, and returns a list
+rstrip()	Returns a right trim version of the string
+split()	Splits the string at the specified separator, and returns a list
+splitlines()	Splits the string at line breaks and returns a list
+startswith()	Returns true if the string starts with the specified value
+strip()	Returns a trimmed version of the string
+swapcase()	Swaps cases, lower case becomes upper case and vice versa
+title()	Converts the first character of each word to upper case
+translate()	Returns a translated string
+upper()	Converts a string into upper case
+zfill()	Fills the string with a specified number of 0 values at the beginning
+"""
