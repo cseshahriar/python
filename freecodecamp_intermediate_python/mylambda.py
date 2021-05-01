cf8f5a6463c3495e0a4de145890f0cb1f2f93825
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-01 12:08:47 +0600

    map func

--- freecodecamp_intermediate_python/mylambda.py
+++ freecodecamp_intermediate_python/mylambda.py
@@ -12,4 +12,11 @@ print(pointers2D_sorted)
 pointers2D_sorted = sorted(pointers2D, key=lambda x: x[0] + x[1]) # sort by second value
 print(pointers2D_sorted)
 
-# map 
+# map(func, seq)
+a = [1,2,3,4,5]
+b = list(map(lambda x: x * 2, a)) # each el multiply by 2
+print(b)
+
+# list comprehension
+c = [x*2 for x in a]
+print(c)
