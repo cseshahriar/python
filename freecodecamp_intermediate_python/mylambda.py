92ecbd4c422db3208e63261e7a0d55df185ebf4c
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-01 12:16:07 +0600

    filter function

--- freecodecamp_intermediate_python/mylambda.py
+++ freecodecamp_intermediate_python/mylambda.py
@@ -12,11 +12,20 @@ print(pointers2D_sorted)
 pointers2D_sorted = sorted(pointers2D, key=lambda x: x[0] + x[1]) # sort by second value
 print(pointers2D_sorted)
 
-# map(func, seq)
+# map(func, seq) : The function to execute for each item
 a = [1,2,3,4,5]
 b = list(map(lambda x: x * 2, a)) # each el multiply by 2
 print(b)
 
-# list comprehension
+# same with list comprenshion
 c = [x*2 for x in a]
 print(c)
+
+# filter(fubc, seq): A Function to be run for each item in the iterable
+f = [1,2,3,4,5,6]
+filter_data = list(filter(lambda x: x % 2 == 0, f)) # return true, false
+print(filter_data)
+
+# same with list comprenshion
+list_data = [x for x in f if x % 2 == 0]
+print(list_data)
