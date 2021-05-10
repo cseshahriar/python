21a433b8783ffee80e257b29020142977a6a5795
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-10 13:00:45 +0600

    py dict to json object

--- freecodecamp_intermediate_python/pyjson.py
+++ freecodecamp_intermediate_python/pyjson.py
@@ -1,1 +1,26 @@
-# Json: 2:41:59
+# Json: javascript object notation
+""" 
+Python-----------Json
+dict------------object
+list,tuple------array
+str-------------string
+int,long,float---number
+True-------------true
+False------------false
+None-------------null
+"""
+import json
+
+# dict
+person = {
+    "first_name": "Shahriar",
+    "last_name": "Hosen",
+    "hobbies": ["running", "swimming", "singing"],
+    "age": 28,
+    "hasChildren": False,
+    "titles": ["Engineer", "Programmer"]
+}
+
+# py dict to json(serialization or encoding)
+personJson = json.dumps(person, indent=4, sort_keys=True) # separators=('; ', '= ')
+print(personJson)
