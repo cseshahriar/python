a78f98ae8b26b70e463342278858eb130ea82aea
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-11 17:34:40 +0600

    decorator more excercise

--- freecodecamp_intermediate_python/decorator_why.py
+++ freecodecamp_intermediate_python/decorator_why.py
@@ -1,5 +1,9 @@
-# decorator act as a wrapper for your original func
-# common use casees of decorators are 1. loggin 2. timing
+""" 
+A decorator is a design pattern in Python that allows a user to 
+add new functionality to an 
+existing object without modifying its structure.
+Decorators are usually called before the definition of a function you want to decorate.
+"""
 import time
 
 def time_it(func):
@@ -29,3 +33,43 @@ def calc_cube(numbers):
 array = range(1, 100000)
 cal_square(array)
 calc_cube(array)
+
+
+
+def uppercase_decorator(function):
+    """ make uppercase """
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+    
+    return wrapper
+
+def split_string(function):
+    def wrapper():
+        func = function()
+        splitted_string = func.splite()
+        return splitted_string
+
+    return wrapper
+
+@split_string
+@uppercase_decorator
+def say_hi():
+    print('Hello World')
+
+say_hi()
+
+
+def decorator_with_arguments(function):
+    def wrapper_accepting_arguments(arg1, arg2):
+        print("My arguments are: {0}, {1}".format(arg1,arg2))
+        function(arg1, arg2)
+    return wrapper_accepting_arguments
+
+
+@decorator_with_arguments
+def cities(city_one, city_two):
+    print("Cities I love are {0} and {1}".format(city_one, city_two))
+
+cities("Nairobi", "Accra")
