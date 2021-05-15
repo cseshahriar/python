7253cf777719c055e82ea20f3aaee2fa44d33914
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-15 14:21:22 +0600

    instance methods

--- freecodecamp_intermediate_python/oop/klass.py
+++ freecodecamp_intermediate_python/oop/klass.py
@@ -9,7 +9,6 @@ class SoftwareEngibeer:
     # class attributes
     alias = "Keyword Magician"
 
-
     def __init__(self, name, age, lavel, salary):
         # instance attributes
         self.name = name
@@ -17,6 +16,16 @@ class SoftwareEngibeer:
         self.level = lavel
         self.salary = salary
 
+    # instance methods
+    def code(self):
+        print(f'{self.name} writing code...')
+
+    def code_in_languages(self, language):
+        print(f'{self.name} is writing code in {language}')
+
+    def info(self):
+        info = f'name = {self.name}, age = {self.age}, level = {self.level}, salary = {self.salary}'
+        return info
 
 # instance
 se1 = SoftwareEngibeer('Shahriar', 28, "Junior", 20000)
@@ -29,3 +38,6 @@ print(f'name: {se1.name}, age: {se1.age}, level: {se1.level}, salary: {se1.salar
 print(se1.alias)
 print(SoftwareEngibeer.alias)
 # print(SoftwareEngibeer.name) # can't
+print(se1.info())
+se1.code()
+se1.code_in_languages('Python')
