a827340963db4b39f11102e98e71ff2626cb5906
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-15 14:32:11 +0600

    special method

--- freecodecamp_intermediate_python/oop/klass.py
+++ freecodecamp_intermediate_python/oop/klass.py
@@ -23,12 +23,19 @@ class SoftwareEngibeer:
     def code_in_languages(self, language):
         print(f'{self.name} is writing code in {language}')
 
-    def info(self):
+    # special methods(every object already has that or builtin)
+    # dunder methods
+    def __str__(self):
         info = f'name = {self.name}, age = {self.age}, level = {self.level}, salary = {self.salary}'
         return info
 
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+
 # instance
 se1 = SoftwareEngibeer('Shahriar', 28, "Junior", 20000)
+se2 = SoftwareEngibeer('Shahriar', 28, "Junior", 20000)
 
 # access instance attributes
 print(f'name: {se1.name}, age: {se1.age}, level: {se1.level}, salary: {se1.salary}')
@@ -38,6 +45,9 @@ print(f'name: {se1.name}, age: {se1.age}, level: {se1.level}, salary: {se1.salar
 print(se1.alias)
 print(SoftwareEngibeer.alias)
 # print(SoftwareEngibeer.name) # can't
-print(se1.info())
+
+print(se1)
 se1.code()
 se1.code_in_languages('Python')
+
+print(se1 == se2) # true
