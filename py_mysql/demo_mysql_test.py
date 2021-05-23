18b61a6dc99cf635a7266c37193b24d4b2ba64ef
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-24 00:41:35 +0600

    insert into

--- py_mysql/demo_mysql_test.py
+++ py_mysql/demo_mysql_test.py
@@ -21,3 +21,37 @@ mycursor = mydb.cursor()
 # mycursor.execute("SHOW DATABASES")
 # for x in mycursor:
 #     print(x)
+
+# create table
+# mycursor.execute("CREATE TABLE customers(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+# alter table
+# mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+# insert into table
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = ("John", "Highway 21")
+# mycursor.execute(sql, val)
+# mydb.commit()
+# print(mycursor.rowcount, "record inserted.")
+
+# Insert Multiple Rows
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = [
+#   ('Peter', 'Lowstreet 4'),
+#   ('Amy', 'Apple st 652'),
+#   ('Hannah', 'Mountain 21'),
+#   ('Michael', 'Valley 345'),
+#   ('Sandy', 'Ocean blvd 2'),
+#   ('Betty', 'Green Grass 1'),
+#   ('Richard', 'Sky st 331'),
+#   ('Susan', 'One way 98'),
+#   ('Vicky', 'Yellow Garden 2'),
+#   ('Ben', 'Park Lane 38'),
+#   ('William', 'Central st 954'),
+#   ('Chuck', 'Main Road 989'),
+#   ('Viola', 'Sideway 1633')
+# ]
+# mycursor.executemany(sql, val)
+# mydb.commit()
+# print(mycursor.rowcount, "was inserted.")
