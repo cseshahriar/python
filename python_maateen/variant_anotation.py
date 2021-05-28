be67db0718125dbdc0735ecfcf01400214d7b538
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-29 00:30:34 +0600

    variant

---
+++ python_maateen/variant_anotation.py
@@ -0,0 +1,12 @@
+number: int = 10
+value = 3.1415 # float
+name: str = 'Shahriar' 
+address = 'Dhaka' # str
+is_happy : bool = True
+is_sad = False
+
+from typing import Dict, List, Tuple
+
+numbers: Tuple[int]
+planets = ['Earth', 'Mars', 'Jupiter'] # type: list[str]
+books: Dict[str, str]
